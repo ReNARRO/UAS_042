3dbb6b51ebce59176e697ed9bb3736b3aa0344f2
E Dewobroto <e.dewobroto.ft21@mail.umy.ac.id>
2023-01-11 15:31:33 +0700

    Add project files.

---
+++ UAS_042/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAS_042
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
